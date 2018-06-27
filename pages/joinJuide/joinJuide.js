c2392d350f0bfa9f3a5642115e30e3e83a7c0b5b
linshuqiao <m15801999427@163.com>
2018-06-28 02:44:15 +0800

    xxx

--- pages/joinJuide/joinJuide.js
+++ pages/joinJuide/joinJuide.js
@@ -5,19 +5,122 @@ Page({
    * 页面的初始数据
    */
   data: {
-      imgUrls: [
-        '/public/images/vip_juide.png',
-      ],
-      groupList: [],
-      invited_id: 3183,
-      order: []
+    imgUrls: [
+      '/public/images/vip_juide.png',
+    ],
+    groupList: [],
+    order: []
   },
+  getUserInfo: function (res) {
+      var that = this
+      var token = wx.getStorageSync('token')
+      wx.request({
+        url: 'https://na.bookfan.cn/api/mini/user/order_vip',
+        data: {
+          userId: wx.getStorageSync('userId'),
+          title:'书友会VIP开通',
+        },
+        header: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + token,//Bearer后面要加空格
+        },
+        method: "POST",
+        success: function (res) {
+          if (res.data.code === 200) {
+            that.setData({
+              order: res.data.data
+            })
+            that.pay();
+          } else {
+            wx.showToast({
+              title: res.data.message,
+              icon: 'none'
+            })
+          }
+        }
+      })
+    },
+  pay: function () {
+    var that = this
+    var order = that.data.order
+    wx.login({
+      success: res => {
+        var token = wx.getStorageSync('token')
+        wx.request({
+          url: 'https://na.bookfan.cn/api/mini/program/pay',
+          data: {
+            code: res.code,
+            title: '书友会vip开通',
+            number: order.order_number,
+            total_fee: order.price
+          },
+          header: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + token,//Bearer后面要加空
+          },
+          method: "POST",
+          success: function (res) {
+            wx.requestPayment({
+              timeStamp: res.data.timeStamp,
+              nonceStr: res.data.nonceStr,
+              package: res.data.package,
+              signType: 'MD5',
+              paySign: res.data.paySign,
+              success: function (res) {
+                wx.showToast({
+                  title: '支付成功！',
+                })
+                that.updatePayStatus()
+              },
+              fail: function (res) {
+                wx.showToast({
+                  title: '取消支付！',
+                })
+                that.destroyOrder()
+              },
+              complete: function (res) {
 
+              },
+            })
+          }
+        })
+      }
+    })
+  },
+  //支付成功，更新订单支付状态
+  updatePayStatus: function () {
+    var that = this;
+    var userId = wx.getStorageSync('userId');
+    var token = wx.getStorageSync('token')
+    wx.request({
+      url: 'https://na.bookfan.cn/api/mini/user/update_order',
+      data: {
+        order: that.data.order.order_number,
+        invited_id: 0,
+        userId: userId
+      },
+      header: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + token,//Bearer后面要加空格
+      },
+      method: "POST",
+      success: function (res) {
+        wx.navigateTo({
+          url: '/pages/invitation/invitation',
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this
+    var userId = options.userId
+    that.setData({
+      isAuth: wx.getStorageSync('isAuth'),
+
+    })
   },
 
   /**
